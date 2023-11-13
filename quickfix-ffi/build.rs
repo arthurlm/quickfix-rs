25237b2a4dce8341b55e1b21091464eb074a9bde
Arthur LE MOIGNE <arthur.lemoigne@gmail.com>
2023-11-13 11:56:36 +0100

    ♻️ Build quickfix / quickfix-bind from quickfix-ffi project

--- quickfix-ffi/build.rs
+++ quickfix-ffi/build.rs
@@ -1,10 +1,39 @@
-use std::{env, path::PathBuf};
+use std::env;
+
+use cmake::Config;
 
 fn main() {
-    let root_path = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
-    let target_path = root_path.parent().expect("No target dir");
-    let link_dir = target_path.join("target").join("quickfix-bind");
+    // Build quickfix as a static library
+    let quickfix_dst = Config::new("../libquickfix")
+        .define("HAVE_PYTHON", "OFF")
+        .define("HAVE_PYTHON3", "OFF")
+        .define("QUICKFIX_SHARED_LIBS", "OFF")
+        .define("QUICKFIX_EXAMPLES", "OFF")
+        .define("QUICKFIX_TESTS", "OFF")
+        .build();
 
-    println!("cargo:rustc-link-search={}", link_dir.as_path().display());
-    println!("cargo:rustc-link-lib=quickfixbind");
+    let quickfix_include_path = format!("{}/include", quickfix_dst.display());
+    let quickfix_lib_path = format!("{}/lib", quickfix_dst.display());
+
+    // Build quickfix C bind also as a static library.
+    env::set_var("CMAKE_LIBRARY_PATH", [quickfix_lib_path].join(";"));
+
+    let quickfix_bind_dst = Config::new("..")
+        .cflag(format!("-I{quickfix_include_path}"))
+        .cxxflag(format!("-I{quickfix_include_path}"))
+        .define("QUICKFIX_BIND_EXAMPLES", "OFF")
+        .build();
+
+    // Configure rustc.
+    println!(
+        "cargo:rustc-link-search=native={}/lib",
+        quickfix_dst.display()
+    );
+    println!(
+        "cargo:rustc-link-search=native={}/lib",
+        quickfix_bind_dst.display()
+    );
+    println!("cargo:rustc-link-lib=static=quickfix");
+    println!("cargo:rustc-link-lib=static=quickfixbind");
+    println!("cargo:rustc-link-lib=stdc++");
 }
