0a945b4c53bbf113d1cdc9584c35376f18dda4e3
Arthur LE MOIGNE <arthur.lemoigne@gmail.com>
2023-11-15 13:08:24 +0100

    🩹 Add missing type export

--- quickfix/src/lib.rs
+++ quickfix/src/lib.rs
@@ -17,11 +17,14 @@ pub use application::{Application, ApplicationCallback};
 pub use error::QuickFixError;
 pub use file_log_factory::FileLogFactory;
 pub use file_store_factory::FileStoreFactory;
+pub use group::Group;
+pub use header::Header;
 pub use message::Message;
 pub use session_id::SessionId;
 pub use session_settings::SessionSettings;
 pub use socket_acceptor::SocketAcceptor;
 pub use socket_initiator::SocketInitiator;
+pub use trailer::Trailer;
 
 pub trait ConnectionHandler {
     fn start(&mut self) -> Result<(), QuickFixError>;
