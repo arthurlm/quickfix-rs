8f2a00341125aa2af927ea906e23e32fec228f39
Arthur LE MOIGNE <arthur.lemoigne@gmail.com>
2023-11-15 10:50:39 +0100

    ✅ Add tests on message header / trailer

--- quickfix/tests/test_messages.rs
+++ quickfix/tests/test_messages.rs
@@ -87,3 +87,53 @@ fn test_remove_field() {
     msg.remove_field(42).unwrap();
     assert_eq!(msg.get_field(42), None);
 }
+
+#[test]
+fn test_get_header() {
+    let mut msg = Message::try_new().unwrap();
+    msg.set_field(40000, "foo").unwrap();
+
+    // Set some header value.
+    assert_eq!(msg.with_header(|x| x.get_field(50000)).unwrap(), None);
+
+    msg.with_header_mut(|x| {
+        x.set_field(50000, "hello").unwrap();
+    })
+    .unwrap();
+
+    assert_eq!(
+        msg.with_header(|x| x.get_field(50000)).unwrap().as_deref(),
+        Some("hello")
+    );
+
+    // Check full message
+    assert_eq!(
+        msg.as_string().as_deref(),
+        Ok("9=22\u{1}50000=hello\u{1}40000=foo\u{1}10=152\u{1}")
+    )
+}
+
+#[test]
+fn test_get_trailer() {
+    let mut msg = Message::try_new().unwrap();
+    msg.set_field(40000, "foo").unwrap();
+
+    // Set some trailer value.
+    assert_eq!(msg.with_trailer(|x| x.get_field(50001)).unwrap(), None);
+
+    msg.with_trailer_mut(|x| {
+        x.set_field(50001, "bar").unwrap();
+    })
+    .unwrap();
+
+    assert_eq!(
+        msg.with_trailer(|x| x.get_field(50001)).unwrap().as_deref(),
+        Some("bar")
+    );
+
+    // Check full message
+    assert_eq!(
+        msg.as_string().as_deref(),
+        Ok("9=20\u{1}40000=foo\u{1}50001=bar\u{1}10=184\u{1}")
+    )
+}
