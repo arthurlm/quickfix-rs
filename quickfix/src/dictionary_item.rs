44d317d5f8692338da528cd1c15c5102a1204df0
Arthur LE MOIGNE <arthur.lemoigne@gmail.com>
2024-03-20 10:18:22 +0100

    ♻️ Reorder dictionary_item impl

--- quickfix/src/dictionary_item.rs
+++ quickfix/src/dictionary_item.rs
@@ -18,62 +18,16 @@ pub trait DictionaryItem {
     fn apply_param(&self, dict: &mut Dictionary) -> Result<(), QuickFixError>;
 }
 
-/// Session connection type.
-pub enum ConnectionType {
-    /// Session is acceptor.
-    Acceptor,
-
-    /// Session is initiator.
-    Initiator,
-}
-
-/// Acceptor port.
-pub struct SocketAcceptPort(pub u32);
-
-/// Initiator port to connect to.
-pub struct SocketConnectPort(pub u32);
-
-/// Initiator host to connect to.
-pub struct SocketConnectHost<'a>(pub &'a str);
-
-/// Time in second to wait before reconnect.
-pub struct ReconnectInterval(pub u16);
-
-/// FIX heartbeat time in seconds.
-pub struct HeartBtInt(pub u16);
-
-/// Session start time.
-pub struct StartTime<'a>(pub &'a str);
-
-/// Session end time.
-pub struct EndTime<'a>(pub &'a str);
-
-/// Application version ID.
-pub struct DefaultApplVerID<'a>(pub &'a str);
-
-/// Data dictionary XML spec path.
-pub struct DataDictionary<'a>(pub &'a str);
-
-/// Transport data dictionary XML spec path.
-pub struct TransportDataDictionary<'a>(pub &'a str);
-
-/// Message store path.
-pub struct FileStorePath<'a>(pub &'a str);
-
-impl DictionaryItem for ConnectionType {
-    fn apply_param(&self, dict: &mut Dictionary) -> Result<(), QuickFixError> {
-        dict.set(
-            "ConnectionType",
-            match self {
-                Self::Acceptor => "acceptor",
-                Self::Initiator => "initiator",
-            },
-        )
-    }
-}
-
 macro_rules! impl_dictionary_item {
     ($item:tt) => {
+        impl DictionaryItem for $item {
+            fn apply_param(&self, dict: &mut Dictionary) -> Result<(), QuickFixError> {
+                dict.set(stringify!($item), self.0)
+            }
+        }
+    };
+
+    ($item:tt as String) => {
         impl DictionaryItem for $item<'_> {
             fn apply_param(&self, dict: &mut Dictionary) -> Result<(), QuickFixError> {
                 dict.set(stringify!($item), self.0.to_string())
@@ -90,17 +44,70 @@ macro_rules! impl_dictionary_item {
     };
 }
 
+/// Session connection type.
+pub enum ConnectionType {
+    /// Session is acceptor.
+    Acceptor,
+
+    /// Session is initiator.
+    Initiator,
+}
+
+impl DictionaryItem for ConnectionType {
+    fn apply_param(&self, dict: &mut Dictionary) -> Result<(), QuickFixError> {
+        dict.set(
+            "ConnectionType",
+            match self {
+                Self::Acceptor => "acceptor",
+                Self::Initiator => "initiator",
+            },
+        )
+    }
+}
+
+/// Acceptor port.
+pub struct SocketAcceptPort(pub u32);
 impl_dictionary_item!(SocketAcceptPort as i32);
+
+/// Initiator port to connect to.
+pub struct SocketConnectPort(pub u32);
 impl_dictionary_item!(SocketConnectPort as i32);
-impl_dictionary_item!(SocketConnectHost);
+
+/// Initiator host to connect to.
+pub struct SocketConnectHost<'a>(pub &'a str);
+impl_dictionary_item!(SocketConnectHost as String);
+
+/// Time in second to wait before reconnect.
+pub struct ReconnectInterval(pub u16);
 impl_dictionary_item!(ReconnectInterval as i32);
+
+/// FIX heartbeat time in seconds.
+pub struct HeartBtInt(pub u16);
 impl_dictionary_item!(HeartBtInt as i32);
-impl_dictionary_item!(StartTime);
-impl_dictionary_item!(EndTime);
-impl_dictionary_item!(DefaultApplVerID);
-impl_dictionary_item!(DataDictionary);
-impl_dictionary_item!(TransportDataDictionary);
-impl_dictionary_item!(FileStorePath);
+
+/// Session start time.
+pub struct StartTime<'a>(pub &'a str);
+impl_dictionary_item!(StartTime as String);
+
+/// Session end time.
+pub struct EndTime<'a>(pub &'a str);
+impl_dictionary_item!(EndTime as String);
+
+/// Application version ID.
+pub struct DefaultApplVerID<'a>(pub &'a str);
+impl_dictionary_item!(DefaultApplVerID as String);
+
+/// Data dictionary XML spec path.
+pub struct DataDictionary<'a>(pub &'a str);
+impl_dictionary_item!(DataDictionary as String);
+
+/// Transport data dictionary XML spec path.
+pub struct TransportDataDictionary<'a>(pub &'a str);
+impl_dictionary_item!(TransportDataDictionary as String);
+
+/// Message store path.
+pub struct FileStorePath<'a>(pub &'a str);
+impl_dictionary_item!(FileStorePath as String);
 
 impl DictionaryItem for (&'static str, &str) {
     fn apply_param(&self, dict: &mut Dictionary) -> Result<(), QuickFixError> {
